62f7b835c432aa51184f7bca8367753341f54899
Patrick Carlton <pcarlton73@gmail.com>
2015-07-17 13:14:09 -0500

    ActiveRecord atomics pre web

---
+++ database_setup.rb
@@ -0,0 +1,26 @@
+unless ActiveRecord::Base.connection.table_exists?(:photographers)
+  ActiveRecord::Base.connection.create_table :photographers do |t|
+    t.text :name
+  end
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:photos)
+  ActiveRecord::Base.connection.create_table :photos do |t|
+    t.text :name
+    t.text :location
+    t.integer :photographer_id
+  end
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:albums)
+  ActiveRecord::Base.connection.create_table :albums do |t|
+    t.text :name
+  end
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:albums_photos)
+  ActiveRecord::Base.connection.create_table :albums_photos, id: false do |t|
+    t.integer :album_id, index: true
+    t.integer :photo_id, index: true
+  end
+end
---
+++ models/photo.rb
@@ -0,0 +1,22 @@
+
+
+class Photo < ActiveRecord::Base
+  belongs_to :Photographer
+  has_and_belongs_to_many :albums
+
+  validates :name, presence: true
+
+  def top_photo?
+    self.albums.length >= 3
+  end
+
+  def self.top_photos
+    top_photos = []
+    self.all.each do |photo|
+      if photo.top_photo?
+        top_photos << photo
+      end
+    end
+    top_photos
+  end
+end
